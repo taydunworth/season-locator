5485a9e9681e7ceeaeb9f3a77774cbc665b3f4bd
Taylor Cox <taylorashtoncox@gmail.com>
2019-02-24 12:43:29 -0500

    Sets up SeasonDisplay component, determines season user is in based on latitude and month of year, and renders message to DOM for winter or summer

--- src/SeasonDisplay.js
+++ src/SeasonDisplay.js
@@ -1,8 +1,18 @@
 import React from 'react';
 
-const SeasonDisplay = () => {
+const getSeason = (lat, month) => {
+    if (month > 2 && month < 9) {
+        return lat > 0 ? 'summer' : 'winter';
+    } else {
+        return lat > 0 ? 'winter' : 'summer';
+    }
+}
+
+const SeasonDisplay = props => {
+    const season = getSeason(props.lat, new Date().getMonth());
+    const seasonText = season === 'winter' ? 'Brr, it is chilly' : 'Whew, it is hot'
     return (
-        <div>Season Display</div>
+        <div><h1>{seasonText}</h1></div>
     )
 }
 
