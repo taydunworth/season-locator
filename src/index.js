3aa6e0176db7cdd1f1392ccfd16da2697626dca1
Taylor Cox <taylorashtoncox@gmail.com>
2019-02-23 20:42:21 -0500

    Updates functional component to class component and uses state to render latitude on DOM

--- src/index.js
+++ src/index.js
@@ -1,16 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-const App = () => {
-    // Gets user's location
-    window.navigator.geolocation.getCurrentPosition(
-        (position) => console.log(position),
-        (err) => console.log(err)
-    );
+class App extends React.Component {
+    constructor(props) {
+        super(props);
 
-    return (
-        <div>Hi there</div>
-    )
+        this.state = { lat: null };
+
+        // Gets user's location
+        window.navigator.geolocation.getCurrentPosition(
+            position => {
+                this.setState({ lat: position.coords.latitude });
+            },
+            err => console.log(err)
+        );
+    }
+
+    render() {
+        return (
+            <div>Latitude: {this.state.lat}</div>
+        )
+    }
 }
 
 ReactDOM.render(
