3cf8eba5b84862d8c741b3498fde535d4d5f9021
Taylor Cox <taylorashtoncox@gmail.com>
2019-02-23 21:11:46 -0500

    Refactors state by removing constructor and moves geolocation finder to a lifecycle method

--- src/index.js
+++ src/index.js
@@ -2,19 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 class App extends React.Component {
-    constructor(props) {
-        super(props);
+    state = { lat: null, errorMessage: '' };
 
-        this.state = { lat: null, errorMessage: '' };
-
-        // Gets user's location
+    componentDidMount() {
         window.navigator.geolocation.getCurrentPosition(
-            position => {
-                this.setState({ lat: position.coords.latitude });
-            },
-            err => {
-                this.setState ({ errorMessage: err.message });
-            }
+            position =>  this.setState({ lat: position.coords.latitude }),
+            err => this.setState ({ errorMessage: err.message })
         );
     }
 
@@ -25,7 +18,7 @@ class App extends React.Component {
         if (!this.state.errorMessage && this.state.lat) {
             return  <div>Latitude: {this.state.lat}</div>;
         } else {
-            return  <div>Latitude: Loading</div>;
+            return  <div>Loading</div>;
         }
     }
 }
