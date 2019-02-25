8ac007488e1a6e40b6590ea711b302f868020a63
Taylor Cox <taylorashtoncox@gmail.com>
2019-02-25 13:22:28 -0500

    Incorporates styles to SeasonDisplay with icons, creates seasonConfig for refactoring purposes, and adds loader for better UX

--- src/SeasonDisplay.js
+++ src/SeasonDisplay.js
@@ -1,4 +1,16 @@
+import './SeasonDisplay.css';
 import React from 'react';
+
+const seasonConfig = {
+    summer: {
+        text: "Let's hit the beach!",
+        iconName: 'sun'
+    },
+    winter: {
+        text: "Brr, it's chilly!",
+        iconName: 'snowflake'
+    }
+}
 
 const getSeason = (lat, month) => {
     if (month > 2 && month < 9) {
@@ -10,9 +22,14 @@ const getSeason = (lat, month) => {
 
 const SeasonDisplay = props => {
     const season = getSeason(props.lat, new Date().getMonth());
-    const seasonText = season === 'winter' ? 'Brr, it is chilly' : 'Whew, it is hot'
+    const { text, iconName } = seasonConfig[season]
+
     return (
-        <div><h1>{seasonText}</h1></div>
+        <div className={`season-display ${season}`}>
+            <i className={`icon-left massive ${iconName} icon`} />
+            <h1>{text}</h1>
+            <i className={`icon-right massive ${iconName} icon`} />
+        </div>
     )
 }
 
